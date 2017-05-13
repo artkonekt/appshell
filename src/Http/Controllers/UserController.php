b409f6ce4e80f6e2ea75a4d180600a611d763ee5
Attila Fulop <fulopattila122@gmail.com>
2017-05-13 16:44:06 +0300

    Added UserController and web route file

---
+++ src/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Contains the UserController class.
+ *
+ * @copyright   Copyright (c) 2017 Attila Fulop
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2017-05-13
+ *
+ */
+
+
+namespace Konekt\AppShell\Http\Controllers;
+
+
+class UserController
+{
+
+}
