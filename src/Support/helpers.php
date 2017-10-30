baf392ff074b139d56987dc2eb0e0f93645ff96c
Attila Fulop <fulopattila122@gmail.com>
2017-10-30 22:10:39 +0200

    Re-added `any_key_exists` method (somehow it has lost)

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -19,3 +19,16 @@ function enum_icon(\Konekt\Enum\Enum $enum)
 {
     return app('appshell.icon')->icon($enum);
 }
+
+/**
+ * Returns whether the given array contains any of the keys
+ *
+ * @param array $haystack   The array to search for the keys
+ * @param array $needles    The list of keys
+ *
+ * @return bool
+ */
+function any_key_exists(array $haystack, array $needles)
+{
+    return count(array_intersect(array_keys($haystack), $needles)) > 0;
+}
