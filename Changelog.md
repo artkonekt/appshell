25c12a7e2a37b62b24050ffcbd5c72b8b755ba8a
Attila Fulop <fulopattila122@gmail.com>
2017-11-24 14:23:15 +0200

    Release v0.9.0

---
+++ Changelog.md
@@ -0,0 +1,8 @@
+# AppShell Changelog
+
+## 0.9
+
+### 0.9.0
+##### 2017-11-24
+
+- Version tracking and Changelog has started
--- src/resources/manifest.php
+++ src/resources/manifest.php
@@ -4,6 +4,6 @@ use Konekt\Concord\Module\Kind;
 
 return [
     'name'    => 'Konekt AppShell Box',
-    'version' => '0.1.0',
+    'version' => '0.9.0',
     'kind'    => Kind::BOX()
 ];
