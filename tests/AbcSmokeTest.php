40911a997c8f23ca11715e50457b3d6d246a3312
Attila Fulop <fulopattila122@gmail.com>
2018-07-02 13:52:05 +0200

    Bumped menu component to version 1.1

---
+++ tests/AbcSmokeTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Contains the AbcSmokeTest class.
+ *
+ * @copyright   Copyright (c) 2018 Attila Fulop
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2018-07-02
+ *
+ */
+
+
+namespace Konekt\AppShell\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AbcSmokeTest extends TestCase
+{
+    const MIN_PHP_VERSION = '7.0.0';
+
+    /**
+     * Very Basic smoke test case for testing against parse errors, etc
+     *
+     * @test
+     */
+    public function smoke()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test for minimum PHP version
+     *
+     * @depends smoke
+     * @test
+     */
+    public function php_version_satisfies_requirements()
+    {
+        $this->assertFalse(version_compare(PHP_VERSION, self::MIN_PHP_VERSION, '<'),
+            'PHP version ' . self::MIN_PHP_VERSION . ' or greater is required but only '
+            . PHP_VERSION . ' found.');
+    }
+}
