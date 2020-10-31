43ec888213904891fce22774a7996a970823ecd0
Attila Fulop <fulopattila122@gmail.com>
2020-10-31 14:09:49 +0100

    Added breadcrumbs tests

--- src/resources/views/layouts/default/_breadcrumbs.blade.php
+++ src/resources/views/layouts/default/_breadcrumbs.blade.php
@@ -4,10 +4,7 @@
     {!! Breadcrumbs::render() !!}
 @else
     <nav class="breadcrumb">
-        <span class="breadcrumb-item active">
-            &nbsp;
-        </span>
-
+        <span class="breadcrumb-item active">&nbsp;</span>
         <span class="breadcrumb-menu">
             @yield('breadcrumb-menu')
         </span>
---
+++ tests/Feature/BreadcrumbsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Contains the BreadcrumbsTest class.
+ *
+ * @copyright   Copyright (c) 2020 Attila Fulop
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2020-10-31
+ *
+ */
+
+namespace Konekt\AppShell\Tests\Feature;
+
+use Konekt\AppShell\Tests\TestCase;
+
+class BreadcrumbsTest extends TestCase
+{
+    /** @test */
+    public function the_customer_index_contains_breadcrumbs()
+    {
+        $response = $this->actingAs($this->adminUser)->get(route('appshell.customer.index'));
+
+        $response->assertSee('<nav class="breadcrumb', false);
+        $response->assertSee('class="breadcrumb-item active">Customers', false);
+    }
+}
