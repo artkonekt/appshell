7c7ebb65edd8e5916783aa4348e9b9ada8ad0b72
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2024-02-14 12:03:41 +0200

    Added back custom parameters support to the table actions widget

--- src/resources/views/widgets/table_actions.blade.php
+++ src/resources/views/widgets/table_actions.blade.php
@@ -1,6 +1,6 @@
 @isset($actions['edit'])
     @can($actions['edit']['can'])
-        <a href="{{ route($actions['edit']['route'], $model) }}"
+        <a href="{{ route($actions['edit']['route'], $actions['edit']['route_parameters'] ?? $model) }}"
            class="btn btn-sm btn-outline-primary btn-show-on-tr-hover float-right" title="{{ __('Edit') }}">
             {!! icon('edit') !!}
         </a>
@@ -9,7 +9,7 @@
 
 @isset($actions['delete'])
     @can($actions['delete']['can'])
-        {!! Form::open(['route' => [$actions['delete']['route'], $model],
+        {!! Form::open(['route' => [$actions['delete']['route'], $actions['delete']['route_parameters'] ?? $model],
                         'method' => 'DELETE',
                         'data-confirmation-text' => $actions['delete']['confirmation_text'],
                         'class' => 'd-inline',
